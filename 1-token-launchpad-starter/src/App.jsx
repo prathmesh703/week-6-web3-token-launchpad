4c8fea41ab2321572883c3d4cd6da4ab01bf6748
prathmesh703 <146568950+prathmesh703@users.noreply.github.com>
2025-01-07 22:22:09 +0530

    connection with the wallet

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,50 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import React from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import "./App.css";
+
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+import {
+  WalletDisconnectButton,
+  WalletModalContext,
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 
 function App() {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint= useMemo(()=>clusterApiUrl(network),[network])
   return (
-    <TokenLaunchpad></TokenLaunchpad>
-  )
+  
+    <div style={{width: "100vw"}}>
+    <ConnectionProvider endpoint={ endpoint }>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+        <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+          <WalletMultiButton />
+          <WalletDisconnectButton></WalletDisconnectButton>
+          </div>
+         
+          <div>
+            
+            <TokenLaunchpad />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,5 +1,34 @@
+import { Transaction,SystemProgram, Keypair} from '@solana/web3.js'
+import {useConnection, useWallet} from '@solana/wallet-adapter-react'
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
 export function TokenLaunchpad() {
+
+    const wallet=useWallet();
+    const {connection}=useConnection();
+
+    async function createToken() {
+        const keypair=Keypair.generate();
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+    const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey: wallet.publicKey,
+            newAccountPubkey: keypair.publicKey,
+            space: MINT_SIZE,
+            lamports,
+            programId:TOKEN_PROGRAM_ID,
+        }),
+        createInitializeMint2Instruction(keypair.publicKey, 6, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
+    );
+
+    transaction.feePayer=wallet.publicKey;
+    const recentblockhash=await connection.getLatestBlockhash();
+    transaction.recentBlockhash=recentblockhash.blockhash;
+    transaction.partialSign(keypair)
+    wallet.signTransaction(transaction);
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -12,6 +41,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button className='btn' onClick={createToken}>Create a token</button>
     </div>
 }
